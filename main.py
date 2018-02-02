562de5b6e7696344c442154e6e0e165b0aff6c47
luoy2 <luoy2@hotmail.com>
2018-02-01 21:30:27 -0500

    init

---
+++ main.py
@@ -0,0 +1,83 @@
+import requests
+import json
+import datetime
+import logging
+import logging.handlers
+from aliyunsdkcore import client
+from aliyunsdkalidns.request.v20150109 import DescribeDomainRecordsRequest
+from aliyunsdkalidns.request.v20150109 import DescribeDomainRecordInfoRequest
+from aliyunsdkalidns.request.v20150109 import UpdateDomainRecordRequest
+
+
+
+class DNS(object):
+    logging.basicConfig(level=0)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    fh = logging.handlers.RotatingFileHandler('logs/aliyun_ddns_record.log',
+                                                          mode='a',
+                                                          maxBytes=10 * 1024 * 1024,
+                                                          backupCount=10)
+    formatter = logging.Formatter(
+        '%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    def __init__(self):
+        with open('key.json', 'r') as f:
+            self.s = json.loads(f.read())[0]
+        self.clt = client.AcsClient(self.s['AccessKeyId'], self.s['AccessKeySecret'])
+        self.dns_domain, self.rc_format = self.s['rc_domain'], self.s['rc_format']
+
+    def check_records(self):
+        request = DescribeDomainRecordsRequest.DescribeDomainRecordsRequest()
+        request.set_DomainName(self.dns_domain)
+        request.set_accept_format(self.rc_format)
+        result = self.clt.do_action_with_exception(request)
+        result = result.decode()
+        result_dict = json.JSONDecoder().decode(result)
+        result_list = result_dict['DomainRecords']['Record']
+        for j in result_list:
+            self.logger.info('Subdomain：' + j['RR'] + ' ' + '| RecordId：' + j['RecordId'])
+            if j['RR'] == 'usdb':
+                rc_rr, rc_type, rc_value, rc_record_id, rc_ttl = j['RR'], j[
+                    'Type'], j['Value'], j['RecordId'], j['TTL']
+                return (rc_rr, rc_type, rc_value, rc_record_id, rc_ttl)
+        return ([None] * 5)
+
+
+    def getMyIp(self):
+        try:
+            u = requests.get('https://api.ipify.org/?format=text')
+            return u.text
+        except requests.HTTPError as e:
+            self.logger.exception('getMyIp:' + str(e))
+            return None
+
+    def update_dns(self, rc_rr, rc_type, rc_value, rc_record_id, rc_ttl):
+        self.logger.info(f'about updating ip : {rc_value}')
+        request = UpdateDomainRecordRequest.UpdateDomainRecordRequest()
+        request.set_RR(rc_rr)
+        request.set_Type(rc_type)
+        request.set_Value(rc_value)
+        request.set_RecordId(rc_record_id)
+        request.set_TTL(rc_ttl)
+        request.set_accept_format(self.rc_format)
+        result = self.clt.do_action_with_exception(request)
+        self.logger.info(result)
+
+
+
+    def worker(self):
+        rc_rr, rc_type, rc_value, rc_record_id, rc_ttl = self.check_records()
+        cur_ip = self.getMyIp()
+        if cur_ip != rc_value:
+            self.update_dns()
+
+
+
+
+if __name__ =='__main__':
+    d = DNS()
+    d.worker()
+
